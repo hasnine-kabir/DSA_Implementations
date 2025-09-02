23ecd5890dfc0ca64a3972399cc9b6acd3962c94
hasnine-kabir <hasnainkabir11@gmail.com>
2025-09-03 01:51:52 +0600

    temp

---
+++ ArrStr/sl.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define revAll(x) sort((x).rbegin(), (x).rend())
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl "\n"
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+int main() {
+    fastio;
+
+    int t = 1;
+    // cin >> t; // Uncomment if multiple test cases
+    while(t--) {
+        
+        // Your code here
+        
+    }
+
+    return 0;
+}
